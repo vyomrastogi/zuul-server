45ab38d2f6fd48a2322597583febaabc1770281f
Vyom Rastogi <vyom.reloaded@gmail.com>
2018-06-29 08:19:45 -0400

    first draft

---
+++ src/main/java/com/vyom/payroll/microservices/ZuulLoggingFilter.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.vyom.payroll.microservices;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * @author Vyom
+ *
+ */
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private static final Logger LOG  = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		// TODO Auto-generated method stub
+		RequestContext context = RequestContext.getCurrentContext();
+		HttpServletRequest request = context.getRequest();
+		
+		LOG.info(String.format("%s Request is : %s", request.getMethod(),request.getRequestURL().toString()));
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
